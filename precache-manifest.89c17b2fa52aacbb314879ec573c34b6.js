473a191a025178e53e053edbddf5824ab0bf527f
kmurase <kmurase@pfrobotics.jp>
2023-08-14 16:38:27 +0900

    Updates

---
+++ precache-manifest.89c17b2fa52aacbb314879ec573c34b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34a4e2b85dbdc6b5d3bc",
+    "url": "/textcode/static/css/main.31e82ca2.chunk.css"
+  },
+  {
+    "revision": "34a4e2b85dbdc6b5d3bc",
+    "url": "/textcode/static/js/main.34a4e2b8.chunk.js"
+  },
+  {
+    "revision": "f5e64fe23ca45f9fa1db",
+    "url": "/textcode/static/js/1.f5e64fe2.chunk.js"
+  },
+  {
+    "revision": "00d42803dacacf2bf466",
+    "url": "/textcode/static/js/2.00d42803.chunk.js"
+  },
+  {
+    "revision": "71ff181829155e95d21d",
+    "url": "/textcode/static/js/runtime~main.71ff1818.js"
+  },
+  {
+    "revision": "05229106718e9f5b7198737c30eb4ec5",
+    "url": "/textcode/index.html"
+  }
+];
